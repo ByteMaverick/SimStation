4be96345c152c60732596ec767a5513f6265601d
Mohammed <ibytemaverick@gmail.com>
2025-04-12 21:21:41 -0700

    initial commit

---
+++ src/SimStation/Agent.java
@@ -0,0 +1,85 @@
+package SimStation;
+
+import java.io.Serializable;
+
+public  abstract  class Agent implements Runnable, Serializable {
+
+    int xc ;
+    int yc;
+    boolean paused;
+    boolean stopped;
+    String agentName;
+    Thread myThread;
+
+    public Agent(String name) {
+        this.agentName = name;
+        this.xc = 0;
+        this.yc = 0;
+        stopped = false;
+        paused = false;
+        myThread = null;
+    }
+
+
+    public synchronized void start(){
+        if (myThread == null){
+            myThread = new Thread(this);
+            myThread.start();
+        }
+    }
+
+    public synchronized void stop(){
+        stopped = true;
+        myThread.interrupt();
+    }
+
+    public synchronized void pause(){
+        paused = true;
+    }
+
+    public synchronized void resume(){
+        paused = false;
+        notify();
+
+    }
+
+    @Override
+    public void run() {
+        onStart();
+
+        try {
+            while (!stopped) {
+                synchronized (this) {
+                    while (paused) {
+                        wait(); // Efficient waiting
+                    }
+                }
+
+                update(); // Agent's behavior
+                Thread.sleep(20); // Smooth pacing
+            }
+        } catch (InterruptedException e) {
+            onInterrupted();
+        }
+
+        onExit();
+    }
+
+    // Lifecycle hooks
+    protected void onStart() {}
+    protected void onInterrupted() {}
+    protected void onExit() {}
+
+    public abstract void update(); // Must be implemented by subclasses
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/SimStation/World.java
@@ -0,0 +1,16 @@
+package SimStation;
+
+import mvc.*;
+
+import java.util.ArrayList;
+
+public class World extends Model {
+    int SIZE = 500;
+    int clock = 0;
+    int alive = 0;
+    ArrayList <Integer> agents = new ArrayList<>();
+
+
+
+
+}
